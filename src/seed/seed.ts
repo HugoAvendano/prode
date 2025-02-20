5483cebff2c163918a8511296246969befc8d9bd
Hugo Avendaño <hugoavendano2014@gmail.com>
2025-02-20 16:36:14 -0300

    Modificacion .gitignore
    Se evita commitear carpeta generada por prisma /postgres/

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -20,6 +20,11 @@ interface SeedData {
 
 }
 
+interface SeedData {
+  dates_fixture: date_fixture[];
+
+}
+
 export const initialData: SeedData = { 
   dates_fixture: [
     {
@@ -104,10 +109,97 @@ export const initialData: SeedData = {
       ]
     }
   ]
-
-
 }
-
-
-
-
+export const teams = [
+  {
+    name: "Aldosivi",
+    url_image: "aldosivi.png"
+  },{
+    name: "Argentinos Jrs",
+    url_image: "argentinos.png"
+  },{
+    name: "Atl. Tucuman",
+    url_image: "atletico_tucuman.png"
+  },{
+    name: "Banfield",
+    url_image: "banfield.png"
+  },{
+    name: "Barracas",
+    url_image: "barracas.png"
+  },{
+    name: "Blegrano",
+    url_image: "belgrano.png"
+  },{
+    name: "Boca Jrs",
+    url_image: "boca.png"
+  },{
+    name: "Central Cordoba",
+    url_image: "central_cordoba.png"
+  },{
+    name: "Defensa y Justicia",
+    url_image: "defensa.png"
+  },{
+    name: "Estudiantes",
+    url_image: "estudiantes.png"
+  },{
+    name: "Gimnasia",
+    url_image: "gimnasia.png"
+  },{
+    name: "Godoy Cruz",
+    url_image: "godoy_cruz.png"
+  },{
+    name: "Huracan",
+    url_image: "huracan.png"
+  },{
+    name: "Independiete",
+    url_image: "independiente.png"
+  },{
+    name: "Independiente Riv.",
+    url_image: "independiente_rivadavia.png"
+  },{
+    name: "Instituto",
+    url_image: "instituto.png"
+  },{
+    name: "Lanus",
+    url_image: "lanus.png"
+  },{
+    name: "Newell's",
+    url_image: "newells.png"
+  },{
+    name: "Platense",
+    url_image: "platense.png"
+  },{
+    name: "Racing",
+    url_image: "racing.png"
+  },{
+    name: "Dep. Riestra",
+    url_image: "riestra.png"
+  },{
+    name: "River Plate",
+    url_image: "river.png"
+  },{
+    name: "Rosario Central",
+    url_image: "central.png"
+  },{
+    name: "San Lorenzo",
+    url_image: "san_lorenzo.png"
+  },{
+    name: "San Martin (SJ)",
+    url_image: "san_martin.png"
+  },{
+    name: "Sarmiento",
+    url_image: "sarmiento.png"
+  },{
+    name: "Talleres",
+    url_image: "talleres.png"
+  },{
+    name: "Tigre",
+    url_image: "tigre.png"
+  },{
+    name: "Union",
+    url_image: "union.png"
+  },{
+    name: "Velez",
+    url_image: "velez.png"
+  },
+];
